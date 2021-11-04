067b2b541350f31d0596eb851cf317448ce985b7
guilherme vitorino <guilhermevitorino.dev@gmail.com>
2021-11-04 17:45:04 -0300

    feat: make migrations to Publication and Categories :sparkles:

---
+++ database/migrations/2021_11_04_203420_add_category_foreign_key_to_publication.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryForeignKeyToPublication extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('publications', function (Blueprint $table) {
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('publications', function (Blueprint $table) {
+            //
+        });
+    }
+}
