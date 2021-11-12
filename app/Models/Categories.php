f9f78ee9b043538dc9c90291bd21fc23e2543b40
guilherme vitorino <guilhermevitorino.dev@gmail.com>
2021-11-12 13:33:01 -0300

    feat: update models Categories and Publication

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -7,5 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
+    protected $fillable = [
+        'id',
+        'title'
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
     use HasFactory;
 }
--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -7,5 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Publication extends Model
 {
+    protected $fillable = [
+        'id',
+        'title',
+        'content',
+        'date',
+        'user_id',
+        'category_id'
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'publications';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
     use HasFactory;
 }
