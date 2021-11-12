5d75fef6ede81f28221dab6b6a1695d23960039f
guilherme vitorino <guilhermevitorino.dev@gmail.com>
2021-11-12 17:11:08 -0300

    test: create factorys to Categorie and Publications

---
+++ database/factories/PublicationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Categories;
+
+class PublicationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence, //Generates a fake sentence
+            'content' => $this->faker->paragraph(5), //generates fake 30 paragraphs
+            'date' => $this->faker->date(),
+            'user_id' => User::factory(), //Generates a User from factory and extracts id
+            'category_id' => Categories::factory(), //Generates a Category from factory and extracts id
+        ];
+    }
+}
